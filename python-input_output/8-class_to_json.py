6665761b2be3e54632a70fd76a5dd3088740d776
jrwaka <j.rwaka@alustudent.com>
2024-03-11 06:26:27 -0700

    Class to JSON

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Defines a function for Python class-to-JSON function"""
+"""Defines a Python function for class-to-JSON"""
 
 
 def class_to_json(obj):
-    """Returns the dictionary representation of a simple data structure"""
-    return obj.___dict___
+    """Return the dictionary representation of a simple data structure"""
+    return obj.__dict__
