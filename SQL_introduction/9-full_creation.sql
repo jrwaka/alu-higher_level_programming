636772baa8bee8e98dafe2ae3f8faaa9b5e9cc7c
jrwaka <j.rwaka@alustudent.com>
2024-03-18 05:34:23 -0700

    Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,7 @@
 -- Script that creates a table second_table in the database hbtn_0c_0
 -- If the table already exists, the script shouldn't fail
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT)
-INSERT INTO second_table VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUES (1, "John", 10);
+INSERT INTO second_table VALUES (2, "Alex", 3);
+INSERT INTO second_table VALUES (3, "Bob", 14);
+INSERT INTO second_table VALUES (4, "George", 8);
