5e7f45c1e4309a4f36f3ff8a665038ce510e9f78
jrwaka <j.rwaka@alustudent.com>
2024-03-18 07:51:43 -0700

    Cheating is bad

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,3 @@
 -- Script that updates the score to Bob to 10 in the table second_table
 -- Not allowed to use Bob's id value, only the name field
-UPDATE TABLE second_table SET score=10 WHERE name= "Bob";
+UPDATE second_table SET score=10 WHERE name= "Bob";
