7f5822b93c0504815025d2d6793ec0e5c9999f1c
jrwaka <j.rwaka@alustudent.com>
2024-03-19 03:45:15 -0700

    Root user

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Script that creates the MySQL server user user_0d_1
+-- The user should have all privileges on a MySQL server
+-- If the user already exists, the script should not fail
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
