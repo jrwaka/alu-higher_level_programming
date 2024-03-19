39d27b2d10177724e792d3655a3f283b98a63c00
jrwaka <j.rwaka@alustudent.com>
2024-03-19 04:29:21 -0700

    Read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- The user should only have SELECT privilege in the database
 -- The user password should be set to user_0d_2_pwd
 -- I both the database and the user exist, the scripts should not fail
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
-CREATE USER IF NOT EXISTS 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT on 'hbtn_0d_2'.* TO 'user_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2 IDENTIFIED BY user_0d_2_pwd;
+GRANT SELECT on hbtn_0d_2.* TO user_0d_2@localhost;
