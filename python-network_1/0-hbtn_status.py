57170073ae54a7a666d643cef0d667419cc6c5a5
jrwaka <j.rwaka@alustudent.com>
2024-03-22 03:27:00 -0700

    What's my status? #0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,13 +2,10 @@
 """A python script that fetches https://intranet.hbtn.io/status using urlib package
 """
 
-
-if __main__ == '__name__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+import urllib.request
+with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as res:
+    content = res.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode('utf-8')))
