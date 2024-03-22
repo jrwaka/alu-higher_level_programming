14ce363287c3779a4af18c422fa36def17f7df51
jrwaka <j.rwaka@alustudent.com>
2024-03-22 03:48:11 -0700

    Response header value #0

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-id found in the response's header"""
+"""Python script that sends a request to the URL and displays the value of the X-Request-id"""
 
 import sys
 import urllib.request
