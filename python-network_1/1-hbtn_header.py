4cb1e888f0a96019c0b0b2ab17f1d1dfda662a2a
jrwaka <j.rwaka@alustudent.com>
2024-03-22 03:38:09 -0700

    Response header value #0

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,8 +5,9 @@
 import sys
 import urllib.request
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-request = urllib.request.Request(url)
-with urllib.request.urlopen(request) as response:
-    print(dict(response.headers).get("X-Request-Id"))
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
