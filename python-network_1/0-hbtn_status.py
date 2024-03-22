e8b883eb3fdf8a2d32d44facb473aa8203548d6d
jrwaka <j.rwaka@alustudent.com>
2024-03-22 03:16:37 -0700

    What's my status? #0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,9 +5,10 @@
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://0.0.0.0:5050/status') as res:
+    with urllib.request.urlopen('https://intranet.hbtn/status') as res:
         content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
