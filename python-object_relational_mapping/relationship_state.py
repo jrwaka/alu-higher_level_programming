d01afb65ccad3f4a228f9deca4c719abad5500c8
jrwaka <j.rwaka@alustudent.com>
2024-03-28 03:54:36 -0700

    City relationship

--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """Contains state class and base"""
-from sqlalchemy import Column, Integer, String, Metadata
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-
 
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
-    """Contains id and name of each state"""
+    """
+    Class with id and name attributes of each state
+    """
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
