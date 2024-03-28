114945bcda6de4446bbd23141d1b4f771e1f64eb
jrwaka <j.rwaka@alustudent.com>
2024-03-28 03:49:35 -0700

    City relationship

---
+++ python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Represents City class"""
+from relatinship_state import Base
+from sqlalchemy import Column, Integer, String, 
+ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class City(Base):
+    """Defines cities"""
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
---
+++ python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Contains state class and base"""
+from sqlalchemy import Column, Integer, String, Metadata
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    """Contains id and name of each state"""
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="states")
