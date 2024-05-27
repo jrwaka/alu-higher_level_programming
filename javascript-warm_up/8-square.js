9a9bc812e8d7476b77266f5f876e1e5013128af8
Junior Rwaka <j.rwaka@alustudent.com>
2024-05-27 03:37:07 -0700

    Square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 const size = parseInt(process.argv[2]);
 if (isNaN(size)) {
-	console.log('Missing size');
-}
-else{
-	for (let i=0; i<size: i++){
-		let j= '';
-		for (let z= 0; z<size; z++){
-			j+='X';
-		}
-		console.log(j);
-	}
-}
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let j = '';
+    for (let z = 0; z < size; z++) {
+      j += 'X';
+    }
+    console.log(j);
+  }
