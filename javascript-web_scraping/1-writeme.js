8ff1dd5686263d4e95a80390fb1f080a578baf33
Junior Rwaka <j.rwaka@alustudent.com>
2024-05-29 04:35:58 -0700

    Write me

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const fs = require('fs);
+
+function writeFile (filePath, content){
+	fs.writeFile(filePath, content, 'utf-8', (err) =>{
+		if (err){
+			console.error(`An error has occured: ${err}`);
+		}
+	});
+}
+
+if (process.argv.length >3){
+	const filePath = process.argv[2];
+	const content = process.argv[3];
+	writeFile(filePath, content);
+}else{
+	console.log('Please provide the file path.');
+}
